ccff35b3f7ea4a32dec42601c37facd2d1e83fde
Boris Adimov <boris@adimov.ru>
2024-07-05 19:45:15 +0700

    dirty hacks

---
+++ dist/constants.d.ts
@@ -0,0 +1,19 @@
+import { Percent } from '@uniswap/sdk-core';
+import JSBI from 'jsbi';
+/**
+ * @deprecated use FACTORY_ADDRESS_MAP instead
+ */
+export declare const FACTORY_ADDRESS = "0x5302086A3a25d473aAbBd0356eFf8Dd811a4d89B";
+export declare const FACTORY_ADDRESS_MAP: {
+    [chainId: number]: string;
+};
+export declare const INIT_CODE_HASH = "0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
+export declare const BASIS_POINTS: JSBI;
+export declare const ZERO_PERCENT: Percent;
+export declare const ONE_HUNDRED_PERCENT: Percent;
